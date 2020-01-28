b83a08043f3070583c6080cf0c5d202315571e9b
kotohayamakawa <zm18425@josai.ac.jp>
2020-01-28 05:39:59 +0000

    01282020

---
+++ ex06-12.js
@@ -0,0 +1,16 @@
+function ThreeSecChange() { 
+ //関数imgchange1()を3000ミリ秒間隔で呼び出す 
+  setInterval("imgchange12()",3000); } 
+   
+
+ function imgchange12() {
+   document.getElementById('logo2').src = './image/snowman.png';
+ }
+
+
+ //      <input type="button" value="画像の変更" onclick="imgchange1();">
+ //  <img src='./image/snowman2.png' id="logo" alt="snowman2 Logo">
+
+ // * ex06-9.js
+ // function imgchange1() {
+ //  document.getElementById('logo').src = './image/snowman2.png';
